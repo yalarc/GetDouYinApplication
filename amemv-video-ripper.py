af8efce211232c44032c7c0f0e57726a25e390a6
yalarc <yalarc@163.com>
2019-10-25 14:58:08 +0800

    v1.0.0提交

--- amemv-video-ripper.py
+++ amemv-video-ripper.py
@@ -1,168 +1,110 @@
-导读：直接上干货，爬取抖音小姐姐视频列表，并去水印下载（仅供学习使用，不做商业用途，如有侵权，联系作者删除）；接18年初，Python基础篇更新。
+# -*- coding: utf-8 -*-
+import codecs
+import copy
+import getopt
+import hashlib
+import json
+import os
+import re
+import sys
+import time
+import urllib
+from threading import Thread
 
-比如我想获取抖音网红“惠子”小姐姐的主页列表视频，第一步在抖音上打开惠子的主页，右上角点击一下，可以看到一个分享按钮，点击分享，找到复制链接->  http://v.douyin.com/9GEGSp/ 。把链接放到浏览器中短链接被自动解析，变成长链接：
-https://www.iesdouyin.com/share/user/73838190950?u_code=128dfi636&sec_uid=MS4wLjABAAAAHmQ4DqHKN8IdfWWd52sYaGS6zaZaOTghOZ4ysZ0z_YM&timestamp=1571884619&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_app_name=douyin ，在长链接中就可以看到一些用户信息，有没有用我们先列出来！
+import requests
+from six.moves import queue as Queue
 
+# Setting timeout
+TIMEOUT = 10
 
-key | value
----|---
-user | 73838190950
-u_code |128dfi636
-sec_uid |MS4wLjABAAAAHmQ4DqHKN8IdfWWd52sYaGS6zaZaOTghOZ4ysZ0z_YM
-timestamp |1571884619
-utm_source |copy
-utm_campaign |client_share
-utm_medium |android
-share_app_name |douyin
+# Retry times
+RETRY = 5
 
-打开浏览器开发者工具，找到对应的视频列表请求接口，一个一个排查终于找到这个链接：https://www.iesdouyin.com/web/api/v2/aweme/post/?sec_uid=MS4wLjABAAAAHmQ4DqHKN8IdfWWd52sYaGS6zaZaOTghOZ4ysZ0z_YM&count=21&max_cursor=0&aid=1128&_signature=QOtJJBARHVwzHUNLqlT-mEDrST&dytk=593d265a74e3384e06112b423ef268da
+# Numbers of downloading threads concurrently
+THREADS = 2
 
-key | value
----|---
-sec_uid | MS4wLjABAAAAHmQ4DqHKN8IdfWWd52sYaGS6zaZaOTghOZ4ysZ0z_YM
-count |21
-max_cursor |1567769380000
-aid |1128
-_signature |F1OCixATSudkpYjkPsX5FRdTgp
-dytk |593d265a74e3384e06112b423ef268da
-
-返回的数据：
-
-```
-     Json:
-{
-"max_cursor": 1569668211000,
-"min_cursor": 1571815003000,
-"has_more": true,
--"extra": {
-"now": 1571888892000,
-"logid": "2019102411481201001404709304158BDD"
-},
-"status_code": 0,
--"aweme_list": [
--{
--"statistics": {
-"aweme_id": "6750893105127378180",
-"comment_count": 1240,
-"digg_count": 30000,
-"play_count": 675000,
-"share_count": 79,
-"forward_count": 17
-},
-"image_infos": null,
-"uniqid_position": null,
-"long_video": null,
-"aweme_id": "6750893105127378180",
-+"text_extra": [ … ],
-"position": null,
-"geofencing": null,
-"promotions": null,
-"desc": "#看啥啥都缺 ，爱买女孩绝不认输。",
-"aweme_type": 4,
-"comment_list": null,
-"video_text": null,
-"cha_list": null,
--"video": {
-+"cover": { … },
-"width": 720,
--"origin_cover": {
--"url_list": [
-"http://p3-dy.byteimg.com/large/tos-cn-p-0015/6e83730009fe4fc2a3eeddbf06b0dbbf_1571815007.jpeg",
-"http://p9-dy.byteimg.com/large/tos-cn-p-0015/6e83730009fe4fc2a3eeddbf06b0dbbf_1571815007.jpeg",
-"http://p1-dy.byteimg.com/large/tos-cn-p-0015/6e83730009fe4fc2a3eeddbf06b0dbbf_1571815007.jpeg"
-],
-"uri": "large/tos-cn-p-0015/6e83730009fe4fc2a3eeddbf06b0dbbf_1571815007"
-},
-"has_watermark": false,
--"play_addr_lowbr": {
-"uri": "v0200ff80000bmnvs5ignbh26fqqufbg",
--"url_list": [
-"https://aweme.snssdk.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=0&ratio=540p&media_type=4&vr_type=0&improve_bitrate=0&is_play_url=1",
-"https://api.amemv.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=1&ratio=540p&media_type=4&vr_type=0&improve_bitrate=0&is_play_url=1"
-]
-},
-"bit_rate": null,
-"vid": "v0200ff80000bmnvs5ignbh26fqqufbg",
--"play_addr": {
-"uri": "v0200ff80000bmnvs5ignbh26fqqufbg",
--"url_list": [
-"https://aweme.snssdk.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=0&ratio=540p&media_type=4&vr_type=0&improve_bitrate=0&is_play_url=1",
-"https://api.amemv.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=1&ratio=540p&media_type=4&vr_type=0&improve_bitrate=0&is_play_url=1"
-]
-},
-"height": 1280,
--"dynamic_cover": {
--"url_list": [
-"https://p3-dy.byteimg.com/obj/tos-cn-p-0015/f4f71ff403574d768a87e7ef3501a7cc_1571815009",
-"https://p9-dy.byteimg.com/obj/tos-cn-p-0015/f4f71ff403574d768a87e7ef3501a7cc_1571815009",
-"https://p1-dy.byteimg.com/obj/tos-cn-p-0015/f4f71ff403574d768a87e7ef3501a7cc_1571815009"
-],
-"uri": "tos-cn-p-0015/f4f71ff403574d768a87e7ef3501a7cc_1571815009"
-},
-"ratio": "540p",
--"download_addr": {
-"uri": "v0200ff80000bmnvs5ignbh26fqqufbg",
--"url_list": [
-"https://aweme.snssdk.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=0&ratio=540p&watermark=0&media_type=4&vr_type=0&improve_bitrate=0&logo_name=aweme_self",
-"https://api.amemv.com/aweme/v1/play/?video_id=v0200ff80000bmnvs5ignbh26fqqufbg&line=1&ratio=540p&watermark=0&media_type=4&vr_type=0&improve_bitrate=0&logo_name=aweme_self"
-]
-},
-"duration": 61824
-},
-"video_labels": null,
-"label_top_text": null
+HEADERS = {
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'upgrade-insecure-requests': '1',
+    'user-agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
 }
-]
-}
-```
-通过返回的参数可以看到我们需要的数据都在这里，在这里不着急解析数据，通过对比请求参数，别的参数都是现成的在主页代码中都可以找到，基本可以确定` _signature `参数是加密字符串，接下来我们就跟踪这个参数的形成过程。通过搜索，确定了它在`index_10ae3b3.js`中生成的 ，截图如下：
 
-通过截图我们知道，`signature` 是通过`_bytedAcrawler`对象获取的，顺着我们查看它的生成过程：截图如下：
+def download(medium_type, uri, medium_url, target_folder):
 
-它是在`base_327cc85.js`生成的,截图如下：
-
-通过分析，`_signature` 获取比较复杂，js代码已经被混淆压制，直接分析算法过程比较难，但是我们可以通过执行签名的算法代码，并返回对应的签名结果。把被压过的js保存下来，执行`_bytedAcrawler.sign("")`获取参数签名。
-
-分析完成后，开始python模拟手机数据请求：
-
-1.读取主页链接：支持同时爬去多个小姐姐的主页视频列表，在`share-url.txt`中输入每个URL通过逗号/空格/tab/表格鍵/回车符 分割，支持多行，也可以使用命令进行指定链接`python amemv-video-ripper.py url1,url2...`，解析文本数据/命令行数据；
-
-```
- content, opts, args = None, None, []
-
-    try:
-        if len(sys.argv) >= 2:
-            opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["favorite"])
-    except getopt.GetoptError as err:
-        usage()
-        sys.exit(2)
-
-    if not args:
-        # check the sites file
-        filename = "share-url.txt"
-        if os.path.exists(filename):
-            content = parse_sites(filename)
-        else:
-            usage()
-            sys.exit(1)
+    headers = copy.copy(HEADERS)
+    file_name = uri
+    if medium_type == 'video':
+        file_name += '.mp4'
+        headers['user-agent'] = 'Aweme/27014 CFNetwork/974.2.1 Darwin/18.0.0'
+    elif medium_type == 'image':
+        file_name += '.jpg'
+        file_name = file_name.replace("/", "-")
     else:
-        content = (args[0] if args else '').split(",")
+        return
 
-    if len(content) == 0 or content[0] == "":
-        usage()
-        sys.exit(1)
+    file_path = os.path.join(target_folder, file_name)
+    if os.path.isfile(file_path):
+        print(file_name + " 已经爬取过了，文件保存在 " + file_path + " 放弃爬取")
+        return
 
-    if opts:
-        for o, val in opts:
-            if o in ("--favorite"):
-                download_favorite = True
-                break
+    print("Downloading %s from %s.\n" % (file_name, medium_url))
+    # VIDEOID_DICT[VIDEO_ID] = 1  # 记录已经下载的视频
+    retry_times = 0
+    while retry_times < RETRY:
+        try:
+            resp = requests.get(medium_url, headers=headers, stream=True, timeout=TIMEOUT)
+            if resp.status_code == 403:
+                retry_times = RETRY
+                print("Access Denied when retrieve %s.\n" % medium_url)
+                raise Exception("Access Denied")
+            with open(file_path, 'wb') as fh:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    fh.write(chunk)
+            break
+        except:
+            pass
+        retry_times += 1
+    else:
+        try:
+            os.remove(file_path)
+        except OSError:
+            pass
+        print("Failed to retrieve %s from %s.\n" % (uri, medium_url))
+        time.sleep(1)
 
-    CrawlerScheduler(content)
-```
-2.获取列表视频：
 
-```
+def get_real_address(url):
+    if url.find('v.douyin.com') < 0:
+        return url
+    res = requests.get(url, headers=HEADERS, allow_redirects=False)
+    return res.headers['Location'] if res.status_code == 302 else None
+
+
+def get_dytk(url):
+    res = requests.get(url, headers=HEADERS)
+    if not res:
+        return None
+    dytk = re.findall("dytk: '(.*)'", res.content.decode('utf-8'))
+    if len(dytk):
+        return dytk[0]
+    return None
+
+
+class DownloadWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            medium_type, uri, download_url, target_folder = self.queue.get()
+            download(medium_type, uri, download_url, target_folder)
+            self.queue.task_done()
+
+
 class CrawlerScheduler(object):
 
     def __init__(self, items):
@@ -183,7 +125,6 @@ class CrawlerScheduler(object):
         self.queue = Queue.Queue()
         self.scheduling()
 
-    #通过node执行fuck-byted-acrawler.js得到签名
     @staticmethod
     def generateSignature(value):
         p = os.popen('node fuck-byted-acrawler.js %s' % value)
@@ -315,6 +256,36 @@ class CrawlerScheduler(object):
             print("Cannot decode response data from DESC %s" % aweme['desc'])
             return
 
+    # def __download_favorite_media(self, user_id, dytk, hostname, signature, favorite_folder, video_count):
+    #     if not os.path.exists(favorite_folder):
+    #         os.makedirs(favorite_folder)
+    #     # favorite_video_url = "https://%s/aweme/v1/aweme/favorite/" % hostname
+    #     favorite_video_url = "https://%s/web/api/v2/aweme/like/" % hostname
+    #     favorite_video_params = {
+    #         'user_id': str(user_id),
+    #         'count': '21',
+    #         'max_cursor': '0',
+    #         'aid': '1128',
+    #         '_signature': signature,
+    #         'dytk': dytk
+    #     }
+    #     max_cursor = None
+    #     while True:
+    #         if max_cursor:
+    #             favorite_video_params['max_cursor'] = str(max_cursor)
+    #         res = requests.get(favorite_video_url,
+    #                            headers=HEADERS, params=favorite_video_params)
+    #         contentJson = json.loads(res.content.decode('utf-8'))
+    #         favorite_list = contentJson.get('aweme_list', [])
+    #         for aweme in favorite_list:
+    #             video_count += 1
+    #             aweme['hostname'] = hostname
+    #             self._join_download_queue(aweme, favorite_folder)
+    #         if contentJson.get('has_more'):
+    #             max_cursor = contentJson.get('max_cursor')
+    #         else:
+    #             break
+    #     return video_count
 
     def _download_user_media(self, user_id, dytk, url):
         current_folder = os.getcwd()
@@ -469,121 +440,69 @@ class CrawlerScheduler(object):
         if video_count == 0:
             print("There's no video in music %s." % music_id)
         return video_count
-```
-3.下载视频：
 
-```
-#下载相关的逻辑
-def download(medium_type, uri, medium_url, target_folder):
 
-    headers = copy.copy(HEADERS)
-    file_name = uri
-    if medium_type == 'video':
-        file_name += '.mp4'
-        headers['user-agent'] = 'Aweme/27014 CFNetwork/974.2.1 Darwin/18.0.0'
-    elif medium_type == 'image':
-        file_name += '.jpg'
-        file_name = file_name.replace("/", "-")
+def usage():
+    print("1. Please create file share-url.txt under this same directory.\n"
+          "2. In share-url.txt, you can specify amemv share page url separated by "
+          "comma/space/tab/CR. Accept multiple lines of text\n"
+          "3. Save the file and retry.\n\n"
+          "Sample File Content:\nurl1,url2\n\n"
+          "Or use command line options:\n\n"
+          "Sample:\npython amemv-video-ripper.py url1,url2\n\n\n")
+    print(u"未找到share-url.txt文件，请创建.\n"
+          u"请在文件中指定抖音分享页面URL，并以 逗号/空格/tab/表格鍵/回车符 分割，支持多行.\n"
+          u"保存文件并重试.\n\n"
+          u"例子: url1,url12\n\n"
+          u"或者直接使用命令行参数指定链接\n"
+          u"例子: python amemv-video-ripper.py url1,url2")
+
+
+def parse_sites(fileName):
+    with open(fileName, "rb") as f:
+        txt = f.read().rstrip().lstrip()
+        txt = codecs.decode(txt, 'utf-8')
+        txt = txt.replace("\t", ",").replace(
+            "\r", ",").replace("\n", ",").replace(" ", ",")
+        txt = txt.split(",")
+    numbers = list()
+    for raw_site in txt:
+        site = raw_site.lstrip().rstrip()
+        if site:
+            numbers.append(site)
+    return numbers
+
+download_favorite = False
+
+if __name__ == "__main__":
+    content, opts, args = None, None, []
+
+    try:
+        if len(sys.argv) >= 2:
+            opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["favorite"])
+    except getopt.GetoptError as err:
+        usage()
+        sys.exit(2)
+
+    if not args:
+        # check the sites file
+        filename = "share-url.txt"
+        if os.path.exists(filename):
+            content = parse_sites(filename)
+        else:
+            usage()
+            sys.exit(1)
     else:
-        return
+        content = (args[0] if args else '').split(",")
 
-    file_path = os.path.join(target_folder, file_name)
-    if os.path.isfile(file_path):
-        print(file_name + " 已经爬取过了，文件保存在 " + file_path + " 放弃爬取")
-        return
+    if len(content) == 0 or content[0] == "":
+        usage()
+        sys.exit(1)
 
-    print("Downloading %s from %s.\n" % (file_name, medium_url))
-    # VIDEOID_DICT[VIDEO_ID] = 1  # 记录已经下载的视频
-    retry_times = 0
-    while retry_times < RETRY:
-        try:
-            resp = requests.get(medium_url, headers=headers, stream=True, timeout=TIMEOUT)
-            if resp.status_code == 403:
-                retry_times = RETRY
-                print("Access Denied when retrieve %s.\n" % medium_url)
-                raise Exception("Access Denied")
-            with open(file_path, 'wb') as fh:
-                for chunk in resp.iter_content(chunk_size=1024):
-                    fh.write(chunk)
-            break
-        except:
-            pass
-        retry_times += 1
-    else:
-        try:
-            os.remove(file_path)
-        except OSError:
-            pass
-        print("Failed to retrieve %s from %s.\n" % (uri, medium_url))
-        time.sleep(1)
+    if opts:
+        for o, val in opts:
+            if o in ("--favorite"):
+                download_favorite = True
+                break
 
-```
-4.其他：
-
-```
-#通过短链接-获取长链接
-def get_real_address(url):
-    if url.find('v.douyin.com') < 0:
-        return url
-    res = requests.get(url, headers=HEADERS, allow_redirects=False)
-    return res.headers['Location'] if res.status_code == 302 else None
-
-# 得到dytk参数
-def get_dytk(url):
-    res = requests.get(url, headers=HEADERS)
-    if not res:
-        return None
-    dytk = re.findall("dytk: '(.*)'", res.content.decode('utf-8'))
-    if len(dytk):
-        return dytk[0]
-    return None
-
-# 下载管理器
-class DownloadWorker(Thread):
-    def __init__(self, queue):
-        Thread.__init__(self)
-        self.queue = queue
-
-    def run(self):
-        while True:
-            medium_type, uri, download_url, target_folder = self.queue.get()
-            download(medium_type, uri, download_url, target_folder)
-            self.queue.task_done()
-```
-
-5.执行截图：
-
-
-
-6.源码获取：
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    CrawlerScheduler(content)
